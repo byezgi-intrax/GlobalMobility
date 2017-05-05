ed9463bfb58134fa634ef2526c4ac9da9a8f6626
ikaim-intrax <ikaim@intraxinc.com>
2017-05-05 11:12:29 -0700

    progress on page

--- japan/js/agency.js
+++ japan/js/agency.js
@@ -15,11 +15,15 @@
         }
     });
 
+
+
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
         target: '#mainNav',
         offset: 54
     });
+
+
 
     // Closes responsive menu when a link is clicked
     $('.navbar-collapse>ul>li>a').click(function() {
